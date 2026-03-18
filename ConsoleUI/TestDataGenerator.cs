1585c24f9836624a43b936c056e826000217b7fe
Nicolai <nicolai.hoiberg@hotmail.com>
2026-03-18 14:41:07 +0100

    Started batch generation of test data

--- ConsoleUI/TestDataGenerator.cs
+++ ConsoleUI/TestDataGenerator.cs
@@ -22,7 +22,7 @@ namespace ConsoleUI
 
             for (int i = 0; i < count; i++)
             {
-                doctors.Add(new Doctor(NameGenerator()));
+                doctors.Add(new Doctor($"Doctor{i}"));
 
                 if (i % 100 == 0)
                 {
@@ -39,7 +39,7 @@ namespace ConsoleUI
 
             for (int i = 0; i < count; i++)
             {
-                patiens.Add(new Patient(NameGenerator(), CprGenerator()));
+                patiens.Add(new Patient($"Patient{i}", CprGenerator()));
 
                 if (i % 100 == 0)
                 {
@@ -68,18 +68,15 @@ namespace ConsoleUI
                     DateTime.Now.AddDays(_random.Next(1,30))
                 );
 
+                if (i % 100 == 0)
+                {
+                    Console.WriteLine($"Generated {i}/{count} consultations...");
+                }
+
                 consultations.Add( consultation1 );
             }
             _context.AddRange(consultations);
-        }
-
-        public string NameGenerator() // It's just a number generator that converts them to string. 
-        {
-            int rng = _random.Next(999);
-
-            string name = rng.ToString();
-
-            return name;
+            _context.SaveChanges();
         }
 
         public string CprGenerator()
