2df3be40be9b8c941d19f294e04ce75b2b28859d
Nicolai <nicolai.hoiberg@hotmail.com>
2026-03-23 12:12:56 +0100

    Started interface stuff for consultations

---
+++ Infrastructure/Repositories/ConsultationRepository.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using UseCases.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class ConsultationRepository : IConsultationRepository
+    {
+        private readonly DoctorsOfficeContext _context;
+        public ConsultationRepository(DoctorsOfficeContext context) 
+        {
+            _context = context;
+        }
+        public async Task<Consultation?> GetByIdAsync(Guid id)
+        {
+            var consultation = _context.Consultations
+                .AsNoTracking()
+                .Where(c => c.Id == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Consultation>> GetByPatientIdAsync(Guid patientId)
+        {
+
+        }
+
+        public async Task<IEnumerable<Consultation>> GetByDoctorIdAsync(Guid doctorId)
+        {
+
+        }
+
+        public async Task AddAsync(Consultation consultation)
+        {
+            await _context.Consultations.AddAsync(consultation);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ UseCases/Interfaces/IConsultationRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using Domain.Entities;
+
+namespace UseCases.Interfaces
+{
+    public interface IConsultationRepository
+    {
+        public Task<Consultation?> GetByIdAsync(Guid id);
+
+        public Task<IEnumerable<Consultation>> GetByPatientIdAsync(Guid patientId);
+
+        public Task<IEnumerable<Consultation>> GetByDoctorIdAsync(Guid doctorId);
+
+        public Task AddAsync(Consultation consultation);
+
+        public Task SaveAsync();
+    }
+}
